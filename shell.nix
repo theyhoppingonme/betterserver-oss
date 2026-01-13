3627cd376e775e98fce8ae0dc6e65c008b3b3376
Alexander Ivanov <saiv46.dev@gmail.com>
2026-01-13 23:21:55 +0800

    I use NixOS btw (added gdb for debugging)

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {}}:
 
 pkgs.mkShell {
-  packages = with pkgs; [ zlib curl.dev SDL2 SDL2_image openssl.dev cmake gnumake clang pkg-config ];
+  packages = with pkgs; [ gdb zlib curl.dev SDL2 SDL2_image openssl.dev cmake gnumake clang pkg-config ];
 }
