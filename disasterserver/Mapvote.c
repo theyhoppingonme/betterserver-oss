cc801f40789f39012a82b0d6949cdc84311869b4
pck <theyhoppingonme@gmail.com>
2026-01-21 15:00:27 +0200

    Update Mapvote.c

--- disasterserver/Mapvote.c
+++ disasterserver/Mapvote.c
@@ -75,7 +75,7 @@ bool mapvote_state_handle(PeerData* v, Packet* packet)
 			v->timeout = 0;
 			Info("%s " LOG_RST "(id %d): %s", v->nickname.value, v->id, msg.value);
 			if (!server_cmd_handle(v->server, server_cmd_parse(&msg), v, &msg))
-				server_broadcast_ex(v->server, packet, true, v->id);
+	      server_broadcast_msg_s(v->server, v->id, msg.value); // chatfix
 
 			break;
 		}
