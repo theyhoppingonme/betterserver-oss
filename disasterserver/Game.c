c8cbb6b0b4e3b330f2b00ee9d7cfcdcd372560b8
pck <theyhoppingonme@gmail.com>
2026-01-21 14:59:51 +0200

    Update Game.c

--- disasterserver/Game.c
+++ disasterserver/Game.c
@@ -1211,7 +1211,7 @@ bool game_state_handletcp(PeerData* v, Packet* packet)
 
 			Info("%s " LOG_RST "(id %d): %s", v->nickname.value, v->id, msg.value);
 			if (!server_cmd_handle(v->server, server_cmd_parse(&msg), v, &msg))
-				server_broadcast_ex(v->server, packet, true, v->id);
+	      server_broadcast_msg_s(v->server, v->id, msg.value); // chatfix
 
 			break;
 		}
